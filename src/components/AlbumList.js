fb70356d741fe42321e17e0c5452aa767a3a17a9
Jaco van Schalkwyk <jaco.schalkwyk@gmail.com>
2017-03-08 21:13:01 +0200

    Added http request for album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+
+    componentWillMount() {
+        
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+                this.setState({albums: responseData});
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
