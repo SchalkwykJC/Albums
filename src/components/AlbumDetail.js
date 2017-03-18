7dbdc08297bdfa3c9c570e26202ce31c8e529750
Jaco van Schalkwyk <jaco.schalkwyk@gmail.com>
2017-03-18 23:09:29 +0200

    Added image to card sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+
+    //Destructure props
+    const {title, artist, thumbnail_image, image} = album;
+    //Destructure styles
+    const {thumbnailContainerStyle ,thumbnailStyle, headerContentStyle, albumImage, albumTitle} = styles;
+
     return (
-        <View>
-            <Card>
-                <Text>{props.album.title}</Text>
-            </Card>
-        </View>
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{uri: thumbnail_image}} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={albumTitle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image style={albumImage} source={{uri: image}} />
+            </CardSection>
+        </Card>
     );
 };
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10
+    },
+    thumbnailStyle: {
+        width: 50,
+        height: 50
+    },
+    albumImage: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    albumTitle: {
+        fontSize: 18
+    }
+}
 
 export default AlbumDetail;
